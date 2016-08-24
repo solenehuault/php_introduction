aef7114fdd450410f3254391d6c9ee0ef3f9f454
solenehuault <solene.huault@gmail.com>
2016-08-24 10:04:45 +0200

    form act on file itself

--- basket_calculation/cible.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$full_price_1 = ($_POST[price1] + $_POST[vat1]) * $_POST[quantity1];
-	$full_price_2 = ($_POST[price2] + $_POST[vat2]) * $_POST[quantity2];
-	$full_price_3 = ($_POST[price3] + $_POST[vat3]) * $_POST[quantity3];
-	$total = $full_price_1 + $full_price_2 + $full_price_3;
-?>
-
-<p>Your basket is up to <?php echo $total ?> $!</p>
--- compare_prices/form.php
+++ compare_prices/form.php
@@ -28,5 +28,23 @@
 			<br />
 			<input class="button" type="submit" value="Compare" />
 		</form>
+		
+		<?php
+			$all_prices = array($_POST[name1] => $_POST[price1], $_POST[name2] =    > $_POST[price2], $_POST[name3] => $_POST[price3], $_POST[name4] => $_    POST[price4]);
+
+			sort($all_prices);
+			$cheap = key($all_prices);
+
+			echo current($all_prices);
+
+			asort($all_prices);
+			$expensive = key($all_prices);
+
+			echo current($all_prices);
+
+			echo "The cheapest product is $cheap, ";
+			echo "and the most expensive is $expensive.";
+		?>
+
 	</body>
 </html>
