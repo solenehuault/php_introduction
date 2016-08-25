e3346cba5dcf0d763676b4d70ae496b936728efa
solenehuault <solene.huault@gmail.com>
2016-08-25 10:05:45 +0200

    exercice5 done

--- validate_a_date/form.php
+++ validate_a_date/form.php
@@ -7,15 +7,34 @@
 	<body>
 		<h1>Validate a date</h1>
 		<form method="post" action="form.php">
-			<input type="date" name="date" placeholder="mm/jj/yyyy"></input>
+			<input type="text" name="date" placeholder="mm/jj/yyyy"></input>
 			<input type="submit" value="is the date valid?"></input>
 		</form>
 
 		<?php
 			$date = $_POST['date'];
 			$length = strlen($date);
+
 			if ($length == 10) {
-				echo "Nice date you got there!";
+				$month = substr($date, 0, 2);
+				$month = intval($month);
+
+				$day = substr($date, 3, 2);
+				$day = intval($day);
+
+				$year = substr($date, 6, 4);
+				$year = intval($year);
+
+				if ($month <= 12 && $month >= 01 &&
+						$day <= 31 && $day >= 01 &&
+						$year <= 2900 && $year >= 1900 && 
+						$date[2] == '/' &&
+						$date[5] == '/') {
+					echo "Nice date you got there!";
+				}
+				else {
+					echo "Invalid date!";
+				}
 			}
 			else {
 				echo "Invalid date";
