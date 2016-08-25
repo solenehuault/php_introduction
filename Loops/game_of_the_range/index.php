891ca9edcaeb2337eef98c28c7be198cad7cf028
solenehuault <solene.huault@gmail.com>
2016-08-25 17:19:23 +0200

    exercice5 début

---
+++ Loops/game_of_the_range/index.php
@@ -0,0 +1,40 @@
+<?php session_start();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Game</title>
+	</head>
+	<body>
+		<h1>Game of the range</h1>
+		<form method="post" action="index.php">
+			<input type"number" name="input"></input>
+			<input type="submit" value="Is that the correct number?"></input>
+		</form>
+
+		<?php
+			$_SESSION['number'] = rand(1, 100);
+			echo $_SESSION['number'];
+			$_SESSION['try'] = 1;
+			if ($_SESSION['try'] <= 8) {
+				if (isset($_POST)) {
+					$_SESSION['input'] = $_POST['input'];
+					if ($_SESSION['number'] < $_SESSION['input']) {
+						echo "<p>The number you seek is smaller than the one you gave...</p>";
+					}
+					elseif ($_SESSION['number'] > $_SESSION['input']) {
+						echo "<p>The number you seek is greater than the one you gave...</p>";
+					}
+					else {
+						echo "Congrats, you guessed the number on your ",$_SESSION['try']," try!";
+					}
+				}
+				$_SESSION['try']++;
+			}
+			else {
+				echo "I'm so really sorry, the number was ",$_SESSION['number'];
+			}
+		?>
+	</body>
+</html>
