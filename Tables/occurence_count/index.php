41708bc3454f9ce44f1e4a749f549a03787c7b70
solenehuault <solene.huault@gmail.com>
2016-08-29 14:43:00 +0200

    occurence count début

---
+++ Tables/occurence_count/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Occurence</title>
+	</head>
+	<body>
+		<h1>Occurence count</h1>
+		<form method="post" action="index.php">
+			<input type="number" name="input" />
+			<input type="submit" value=""/>
+		</form>
+	</body>
+</html>
