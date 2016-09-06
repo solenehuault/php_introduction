cb79ddbc46cffdc7290464b951ce220e060a9cd7
solenehuault <solene.huault@gmail.com>
2016-09-06 14:28:34 +0200

    identification done with pdo

--- PHP_MySQL/identity/index.php
+++ PHP_MySQL/identity/index.php
@@ -1,13 +1,14 @@
 <?php
-	include(config.php);
+	include("config.php");
 ?>
+	<!DOCTYPE html>
+	<html>
 
-<!DOCTYPE html>
-<html>
 	<head>
 		<meta charset="utf-8" />
 		<title>Identity</title>
 	</head>
+
 	<body>
 		<h1>Identity</h1>
 		<form method="post" action="index.php">
@@ -15,21 +16,44 @@
 			<input type="text" name="name" id="name" />
 			<label id="psw">Password: </label>
 			<input type="password" name="psw" id="psw" />
-			<input type="submit" value="Connect" />
-		</form>
-
+			<input type="submit" value="Connect" /> </form>
 		<?php
+		
+		try {
+    	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    	// set the PDO error mode to exception
+    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    	echo "Connected successfully to the database<br />";
+    
+		
 			if (isset($_POST['name']) && isset($_POST['psw'])) {
-				$username = $_POST['name'];
-				$sql = "SELECT password FROM user WHERE username=$username";
-				$result = $conn->query($sql);
-				if ($result == $_POST['psw']) {
-					echo ":)";
+				$name = $_POST['name'];
+				$sql = $conn->query("SELECT password FROM user WHERE username='$name'");
+				$result = $sql->fetch(PDO::FETCH_OBJ);
+				if ($result) {
+					foreach ($result as $psw) {
+						if ($psw == $_POST["psw"]) {
+							echo "Connected";
+						}
+						else {
+							echo "Password incorrect";
+						}
+					}
 				}
 				else {
-					echo ":(";
+					echo "Username isn't in the database.";
 				}
 			}
-		?>
+			else{
+				echo "Unknown values for name or password";
+			}
+		}
+		
+		catch(PDOException $e){
+    	echo "Connection failed: " . $e->getMessage();
+  	}
+		
+	?>
 	</body>
-</html>
+
+	</html>
