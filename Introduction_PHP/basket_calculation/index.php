f2dc9e8e8f49618c78e4d8c83c9eebb8d7f74aed
solenehuault <solene.huault@gmail.com>
2016-09-06 09:58:13 +0200

    real tva calculation

--- Introduction_PHP/basket_calculation/index.php
+++ Introduction_PHP/basket_calculation/index.php
@@ -32,9 +32,9 @@
 		</form>
 
 		<?php
-			$full_price_1 = ($_POST[price1] + $_POST[vat1]) * $_POST[quantity1];
-			$full_price_2 = ($_POST[price2] + $_POST[vat2]) * $_POST[quantity2];
-			$full_price_3 = ($_POST[price3] + $_POST[vat3]) * $_POST[quantity3];
+			$full_price_1 = ($_POST[price1] + ($_POST[price1] * $_POST[vat1] / 100)) * $_POST[quantity1];
+			$full_price_2 = ($_POST[price2] + ($_POST[price2] * $_POST[vat2] / 100)) * $_POST[quantity2];
+			$full_price_3 = ($_POST[price3] + ($_POST[price3] * $_POST[vat3] / 100)) * $_POST[quantity3];
 			$total = $full_price_1 + $full_price_2 + $full_price_3;
 			echo "<p>Your basket is up to $total$.</p>";
 		?>
