6d6866da5b8999e204cfc2b387fe9ffd7960a23e
solenehuault <solene.huault@gmail.com>
2016-09-08 14:32:14 +0200

    Compter le nombre d'occurence, fait

--- Tables/occurence_count/index.php
+++ Tables/occurence_count/index.php
@@ -8,7 +8,24 @@
 		<h1>Occurence count</h1>
 		<form method="post" action="index.php">
 			<input type="number" name="input" />
-			<input type="submit" value=""/>
+			<input type="submit" value="Send"/>
 		</form>
+
+		<?php
+			if (isset($_POST['input'])) {
+				$occurence = array();
+				for ($i = 0 ; $i < strlen($_POST['input']) ; $i++) {
+					$input = $_POST['input'];
+					$num = $input[$i];
+					$occurence[$num] += 1;
+				}
+				ksort($occurence);
+				echo "<ul>";
+				foreach ($occurence as $number => $occ) {
+					echo "<li>".$number." appear ".$occ." time</li>";
+				}
+				echo "</ul>";
+			}
+		?>
 	</body>
 </html>
